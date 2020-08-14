4376a7993973f36471dbe499524e70332f573018
Koray <bkoray@protonmail.com>
2020-08-14 12:43:53 +0300

    Dynamic Torus function added to spinning

--- src/pages/spinning.js
+++ src/pages/spinning.js
@@ -29,23 +29,15 @@ function Box(props) {
     )
 }
 
-function Torusses(){
+function Torusses(props){
+    let count = props.count;
 
-    const TorusGeometry1 = useRef(null);
-    const TorusGeometry2 = useRef(null);
-    const TorusGeometry3 = useRef(null);
-    const TorusGeometry4 = useRef(null);
-    const TorusGeometry5 = useRef(null);
-    const TorusGeometry6 = useRef(null);
-    const TorusGeometry7 = useRef(null);
-    const TorusGeometry8 = useRef(null);
-    const TorusGeometry9 = useRef(null);
-    const TorusGeometry10 = useRef(null);
-    const TorusGeometry11 = useRef(null);
+    const TorusGroup = useRef(null)
 
-    const Torusses = [TorusGeometry1, TorusGeometry2, TorusGeometry3, TorusGeometry4, TorusGeometry5, TorusGeometry6, TorusGeometry7, TorusGeometry8, TorusGeometry9, TorusGeometry10, TorusGeometry11];
+    {/* const Torusses = [TorusGeometry1, TorusGeometry2, TorusGeometry3, TorusGeometry4, TorusGeometry5, TorusGeometry6, TorusGeometry7, TorusGeometry8, TorusGeometry9, TorusGeometry10, TorusGeometry11]; */}
 
     useFrame(({clock}) => {
+        {/*
         var i = 0.01;
 
         Torusses.forEach((torus, index) => {
@@ -56,11 +48,19 @@ function Torusses(){
             if(i = 11){
                 i = 0;
             }
-        });     
+        });
+        */}   
+
+        if(TorusGroup.current){
+            TorusGroup.current.children.forEach((child, i) => {
+                child.rotation.z = (Math.sin(clock.getElapsedTime()) * Math.PI/4 * i);
+            })
+        }
       });
 
     return(
-    <group>
+    <group ref={TorusGroup}>
+        {/*
         <Torus ref={TorusGeometry1} args={[10, 0.2]} rotation={[0, 0, 0]} position={[0, 0, -5]} />
         <Torus ref={TorusGeometry2} args={[10, 0.2]} rotation={[0, 0, 3]} position={[0, 0, -10]} />
         <Torus ref={TorusGeometry3} args={[10, 0.2]} rotation={[0, 0, 6]} position={[0, 0, -15]} />
@@ -72,6 +72,12 @@ function Torusses(){
         <Torus ref={TorusGeometry9} args={[10, 0.1]} rotation={[0, 0, 24]} position={[0, 0, -45]} />
         <Torus ref={TorusGeometry10} args={[10, 0.1]} rotation={[0, 0, 27]} position={[0, 0, -50]} />
         <Torus ref={TorusGeometry11} args={[10, 0.1]} rotation={[0, 0, 30]} position={[0, 0, -55]} />
+        */}
+        {Array(count)
+            .fill()
+            .map((item, i) => (
+                <Torus key={i} args={[10, 0.1]} position={[0, 0, (i + 1) * -5]} />
+            ))}
     </group>);
 }
 
@@ -83,7 +89,7 @@ const Spinning = () => {
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
             <Box />
-            <Torusses />
+            <Torusses count={100}/>
             <Effects />
         </Canvas>
     )
